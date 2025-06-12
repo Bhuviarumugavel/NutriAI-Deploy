18aa2147d1a15dce9aa325e37f42c33cd195a2ba
Bhuviarumugavel <bhuanesahwari2005@gnail.com>
2025-06-12 22:40:59 +0530

    Fix: Switch to CommonJS config and remove geist-font submodule

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['your-domain.com'],
+  experimental: {
+    serverActions: true,
   },
 };
 
